76edad7f39c93359d6ca5debb6e653aa5f195264
Moisés Peláez <moises.jpelaez@gmail.com>
2026-08-30 11:30:53 -0400

    Revert HL Storage implementations

--- Backends/Kore-HL/kha/Storage.hx
+++ Backends/Kore-HL/kha/Storage.hx
@@ -1,41 +1,12 @@
 package kha;
 
-using StringTools;
-
-class KoreStorageFile extends StorageFile {
-	var name: String;
-
-	public function new(name: String) {
-		this.name = name;
-	}
-
-	override public function read(): Blob {
-		var size = 0;
-		final bytes = kinc_storage_file_read(StringHelper.convert(name), size);
-		if (bytes == null) {
-			return null;
-		}
-		return Blob.fromBytes(@:privateAccess new haxe.io.Bytes(bytes, size));
-	}
-
-	override public function write(data: Blob): Void {
-		if (data != null) {
-			kinc_storage_file_write(StringHelper.convert(name), @:privateAccess data.bytes.b, data.length);
-		}
-	}
-
-	@:hlNative("std", "kinc_storage_file_read") static function kinc_storage_file_read(name: hl.Bytes, size: hl.Ref<Int>): Pointer {
-		return null;
-	}
-
-	@:hlNative("std", "kinc_storage_file_write") static function kinc_storage_file_write(name: hl.Bytes, data: Pointer, size: Int): Void {}
-}
+import haxe.io.Bytes;
+import haxe.io.BytesBuffer;
+import haxe.io.BytesData;
 
 class Storage {
 	public static function namedFile(name: String): StorageFile {
-		name = name.replace("\\", ".");
-		name = name.replace("/", ".");
-		return new KoreStorageFile(name);
+		return null;
 	}
 
 	public static function defaultFile(): StorageFile {
--- Backends/Kore-HL/kinc-bridge/kinc.c.h
+++ Backends/Kore-HL/kinc-bridge/kinc.c.h
@@ -1,7 +1,6 @@
 #include <kinc/audio2/audio.h>
 #include <kinc/graphics4/graphics.h>
 #include <kinc/io/filereader.h>
-#include <kinc/io/filewriter.h>
 #include <kinc/log.h>
 #include <kinc/system.h>
 #include <kinc/window.h>
@@ -127,32 +126,4 @@ vbyte *hl_kinc_file_contents(vbyte *name, int *size) {
 	kinc_file_reader_close(&file);
 	hl_blocking(false);
 	return content;
-}
-
-vbyte *hl_kinc_storage_file_read(vbyte *name, int *size) {
-	kinc_file_reader_t file;
-	if (!kinc_file_reader_open(&file, (char *)name, KINC_FILE_TYPE_SAVE)) {
-		return NULL;
-	}
-	hl_blocking(true);
-	int len = (int)kinc_file_reader_size(&file);
-	*size = len;
-	hl_blocking(false);
-	vbyte *content = (vbyte *)hl_gc_alloc_noptr(len);
-	hl_blocking(true);
-	kinc_file_reader_read(&file, content, len);
-	kinc_file_reader_close(&file);
-	hl_blocking(false);
-	return content;
-}
-
-void hl_kinc_storage_file_write(vbyte *name, vbyte *data, int size) {
-	kinc_file_writer_t file;
-	if (!kinc_file_writer_open(&file, (char *)name)) {
-		return;
-	}
-	hl_blocking(true);
-	kinc_file_writer_write(&file, data, size);
-	kinc_file_writer_close(&file);
-	hl_blocking(false);
 }
